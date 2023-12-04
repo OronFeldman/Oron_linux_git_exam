af5b15acd624f48c32b3cfa86bcc9346d75ff132
gitoronf <oronfeldman@gmail.com>
2023-12-04 18:25:16 +0200

    Adding a code line to text_ops.sh to find all lines with the word Calculator in README.sh sort them alpabeticaly in descending order and submiting the result to text file calculator-lines.txt

--- text_ops.sh
+++ text_ops.sh
@@ -8,5 +8,7 @@ grep -o '[0-9]*' README.md | sort -n > output/sorted-numbers.txt
 
 awk '/# FEATURES/,/# GETTING/' README.md | sed 's/# FEATURES//' | sed 's/# GETTING//' | sed 's/STARTED//' > output/features-summary.txt
 
+grep -iw 'Calculator' README.md | sort -r -b -f > output/calculator-lines.txt
+
 
 
